8b2a6b39e2669498b19b66284dd678ecf5e23b44
Frontoge <coltsman1011@gmail.com>
2025-04-02 19:14:43 -0400

    fix dependency install script

--- setup-dependencies.sh
+++ setup-dependencies.sh
@@ -2,6 +2,6 @@
 cd vcpkg || { echo "vcpkg directory not found"; exit 1; }
 
 ./bootstrap-vcpkg.sh
-./vcpkg install boost-serialization
+cd .. && ./vcpkg/vcpkg install
 
 echo "Dependencies installed. You can now build the project."
